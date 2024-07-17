a9ec01581bc5a5fa7acf7e6d84862f676c0ee0ed
Ethan Justice <32603588+EJustice1@users.noreply.github.com>
2024-07-17 14:03:28 -0400

    Time: 0 ms (100%), Space: 14.1 MB (7.43%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        const size_t size = chars.size();
         char lastchar = chars[0];
         int len = 0;
 
         int writeIndex = 0;
         int readIndex = 0;
 
-        for (; readIndex < size; ++readIndex) {
-            if (lastchar != chars[readIndex]) {
+        for (char c : chars) {
+            if (lastchar != c) {
                 chars[writeIndex++] = lastchar;
                 if(len > 1){
                 string cnt = to_string(len);
                 for(char ch : cnt)
                     chars[writeIndex++] = ch;
             }
-                lastchar = chars[readIndex];
+                lastchar = c;
                 len = 1;
             } else
                 ++len;
